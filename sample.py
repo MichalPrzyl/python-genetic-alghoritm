960315579acd7e11fac54b342dc5e16fc4c56c51
Michał Przyłucki <michal.przyl@gmail.com>
2024-02-18 02:03:43 +0100

    Clean up

--- sample.py
+++ sample.py
@@ -1,17 +1,10 @@
 import random
-
 from settings import *
-
 import logging
 
 
 class Sample:
     def __init__(self, number=None):
-        # print(f"number: {number}")
-        # assert isinstance(number, int or None), ('Number arg is not a numvber')
-        # if number:
-        #     if not isinstance(number, float):
-        #         raise ValueError('Number arg is not a numvber')
         self.number = number or self.get_random_number()
         self.points = abs(self.number - WINNER)
 
@@ -20,9 +13,7 @@ class Sample:
 
     def is_mutation_occured(self):
         if not random.randrange(0, 101) > PROBABILTY_OF_MUTATION:
-            # print(f"muttaion occured2")
             return True
-        # print("nonono")
         return False
 
     def mutate(self):
